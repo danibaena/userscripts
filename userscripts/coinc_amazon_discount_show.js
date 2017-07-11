2e5066ba20e979ffc1bfa92bdfa25dd43e04122b
Daniel Baena Hermosa <daniel.baena@machiina.com>
2017-07-11 17:56:36 +0200

    Refactor discount show script

--- userscripts/coinc_amazon_discount_show.js
+++ userscripts/coinc_amazon_discount_show.js
@@ -3,51 +3,44 @@
 // @namespace     danibaena
 // @description   Script to add Coinc Discount Price next to the original price
 // @include       http://www.amazon.es/*
+// @include       https://www.amazon.es/*
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require       https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant         none
 // ==/UserScript==
 
-function addCoincPrice () {
+function addCoincPrice() {
+    var parser = new DOMParser();
+    var itemPriceIds = [
+        "priceblock_ourprice",
+        "priceblock_dealprice",
+        "priceblock_saleprice",
+    ];
 
-    var priceId = "priceblock_ourprice";
-    var priceDealId = "priceblock_dealprice";
-    var priceSaleId = "priceblock_saleprice";
-  
-    if(document.getElementById(priceId)!==null) {
-       var itemPrice = document.getElementById(priceId);    
-    } else {
-        if(document.getElementById(priceDealId)!==null) { 
-           var itemPrice = document.getElementById(priceDealId);
+    var itemPrice = itemPriceIds.map(function(currentValue, index) {
+        return document.getElementById(currentValue);
+    }).filter(function(item) {
+        return ((item !== null) && (item.innerHTML !== "No disponible.") && (item.innerHTML.indexOf("-") === -1));
+    }).reduce(function(previousValue, currentValue) {
+        if(previousValue !== null) {
+            return (getPrice(previousValue) < getPrice(currentValue)) ? previousValue : currentValue;
         } else {
-            var itemPrice = document.getElementById(priceSaleId);
+            return currentValue;
         }
-      
-    }
-    
-    var str = itemPrice.innerHTML;
+    });
 
-    if( ( str !== "No disponible." ) && ( str.indexOf("-") === -1 ) ){
-        var res = str.replace("EUR", "").trim().replace(".","").replace(",",".");
-        res = parseFloat(res);  
-        coincPrice = (res/1.04).toFixed(2).toString().replace(".",",").replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+    var res = getPrice(itemPrice);
+    var coincPrice = (res/1.04).toFixed(2).toString().replace(".",",").replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
 
-        var coincPriceTextSpan = document.createElement("span");
-        coincPriceTextSpan.setAttribute("class", "a-color-secondary a-size-base a-text-left");
-        var text = document.createTextNode("Precio Coinc:")
-        coincPriceTextSpan.appendChild(text);
-        coincPriceTextSpan.style.verticalAlign = "top";
-        coincPriceTextSpan.style.padding = "6px 6px";
+    var coincPriceTextSpan = '<span class="a-color-secondary a-size-base a-text-left" style="vertical-align: top; padding: 6px;">Precio Coinc:</span>';
+    var coincPriceSpan = '<span class="a-size-medium a-color-price" id="coinc_price">EUR ' + coincPrice + '</span>';
 
-        var coincPriceSpan = document.createElement("span");
-        coincPriceSpan.setAttribute("class", "a-size-medium a-color-price");
-        coincPriceSpan.setAttribute("id", "coinc_price");
-        var text2 = document.createTextNode("EUR " + coincPrice);
-        coincPriceSpan.appendChild(text2);
-        
-        itemPrice.appendChild(coincPriceTextSpan);
-        itemPrice.appendChild(coincPriceSpan);
-    }
+    $(itemPrice).append(coincPriceTextSpan);
+    $(itemPrice).append(coincPriceSpan);
+}
+
+function getPrice(itemPrice) {
+    return parseFloat(itemPrice.innerHTML.replace("EUR", "").trim().replace(".","").replace(",","."));
 }
 
 waitForKeyElements ("#priceblock_ourprice", addCoincPrice, false);
