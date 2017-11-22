800c5cc6265a2b6d073894bcc1d3809df4d83840
Daniel Baena Hermosa <daniel.baena@machiina.com>
2017-11-22 16:52:38 +0100

    Update wishlist sum script
    -Add mobile version

--- userscripts/amazon_wishlist_sum.js
+++ userscripts/amazon_wishlist_sum.js
@@ -4,61 +4,118 @@
 // @description   Just a simple script to sum all items and prices in an Amazon.es' Wishlist url
 // @include       http://www.amazon.es/gp/registry/wishlist/*
 // @include       https://www.amazon.es/gp/registry/wishlist/*
-// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @include       https://www.amazon.es/hz/wishlist/ls/*
+// @include       http://www.amazon.es/hz/wishlist/ls/*
+// @require       https://code.jquery.com/jquery-3.2.1.slim.min.js
+// @require       https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant         none
 // ==/UserScript==
 
-var divId = "g-items-btf";
-var productsList = document.getElementById(divId);
-var priceClass = "a-size-base a-color-price a-text-bold";
-var itemPrices = document.getElementsByClassName(priceClass);
-var itemSum = 0;
-var itemCounter = 0;
-var itemNotAvailableCounter = 0;
+function addItemPricesSum() {
+    var itemsList               = $(".g-item-sortable[data-id]").length ? $(".g-item-sortable[data-id]") : $(".awl-item-wrapper[data-price]") ;
+    var priceClass              = "a-price-whole";
+    var paginationID            = "wishlistPagination";
+    var pages                   = document.getElementById(paginationID);
+    var page                    = pages ? "Datos para esta página de la lista:<br>" : "Datos de la lista:<br>";
+    var itemSum                 = 0;
+    var itemCounter             = 0;
+    var itemNotAvailableCounter = 0;
 
-for (i=0; i<itemPrices.length; i++){
-	var str = itemPrices[i].innerHTML.trim();
-	if((str !== 'No disponible') && ( str !== '') ){
-			var res = str.replace("EUR", "");
-			res = res.trim();
-			res = res.replace(".","");
-			res = res.replace(",",".");
-			res = parseFloat(res);
-			itemSum+=res;
-			itemCounter++;
-		} else {
-			itemNotAvailableCounter++;
-		}
+    itemsList.each(function(index , item) {
+        var result = $(item).attr('data-price');
+        result     = result !== '-Infinity' ? parseFloat(result) : result;
+        if(result === '-Infinity') {
+            itemNotAvailableCounter++;
+        } else {
+            itemSum += result;
+            itemCounter++;
+        }
+    });
+
+    itemSum      = itemSum.toFixed(2);
+    itemSumCoinc = (itemSum/1.04).toFixed(2);
+    itemSumCoinc = itemSumCoinc.toString().replace(".",",").replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+    itemSum      = itemSum.toString().replace(".",",").replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+
+    var textList = `
+        <p>${page}</p>
+        <div class="row">
+            <span>Artículos totales:</span>
+            <span class="right">${itemCounter}</span>
+        </div>
+        <div class="row">
+            <span>Artículos no disponibles:</span>
+            <span class="right">${itemNotAvailableCounter}</span>
+        </div>
+        <div class="row">
+            <span>Coste total:</span>
+            <span class="right">${itemSum} €</span>
+        </div>
+        <div class="row">
+            <span>Coste total con COINC:</span>
+            <span class="right">${itemSumCoinc} €</span>
+        </div>
+    `;
+
+    var ruleDivider = '<hr class="a-divider-normal awl-divider" id="scriptlist-divider">';
+    ruleDivider     = isMobile() ? ruleDivider : '';
+
+    var scriptList = `
+        ${ruleDivider}
+        <div class="a-price ${priceClass}" data-a-size="m" data-a-color="base" id="scriptlist">
+            ${textList}
+        </div>
+    `;
+
+    var $scriptList = $('#scriptlist');
+
+    if($scriptList.length) {
+        $scriptList.replaceWith(scriptList);
+    } else {
+        $(scriptList).insertAfter('#endOfListMarker');
+    }
 }
-itemSum = itemSum.toFixed(2);
-itemSumCoinc = (itemSum/1.04).toFixed(2);
-itemSumCoinc = itemSumCoinc.toString().replace(".",",").replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
-itemSum = itemSum.toString().replace(".",",").replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
-
-var scriptList = document.createElement("div");
-scriptList.setAttribute("class", priceClass);
-scriptList.setAttribute("id", "scriptList");
-productsList.appendChild(scriptList);
-
-var list = document.getElementById("scriptList");
-list.style.display = 'flex';
-list.style.justifyContent = 'center';
-list.style.borderTop = '1px solid #DDD';
-list.style.paddingTop = '22px';
-list.style.textAlign = 'center';
-
-var paginationID = "wishlistPagination";
-var pages = document.getElementById(paginationID);
-var page;
 
 
-if(pages !== null){
-    page = "Datos para esta página de la lista:<br>";
-} else {
-    page = "Datos de la lista:<br>";
-}
-list.innerHTML = page +
-         "Artículos totales: "        + itemCounter             + "<br>" +
-         "Artículos no disponibles: " + itemNotAvailableCounter + "<br>" +
-         "Coste total: "              + itemSum                 + " EUR<br>" +
-         "Coste total con COINC: "    + itemSumCoinc            + " EUR";
+var isMobile = function() {
+    return /Mobi/i.test(navigator.userAgent) || /Android/i.test(navigator.userAgent);
+};
+
+var scriptListStyles = `
+    <style>
+        #scriptlist {
+            line-height: 1;
+            font-size: 12px;
+            font-weight: 400;
+            z-index: 2;
+            position: relative;
+            display: block;
+            max-width: 292px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding-top: 15px;
+            ${isMobile() ? 'padding-left: 15px;' : ''}
+        }
+
+        #scriptlist p {
+            font-weight: 600;
+        }
+        #scriptlist .row {
+            display: flex;
+            justify-content: space-between;
+            padding-bottom: 5px;
+        }
+        #scriptlist .row .right {
+            font-weight: 600;
+        }
+
+        #scriptlist-divider {
+            margin-top: 15px;
+        }
+    </style>
+`;
+
+$(scriptListStyles).appendTo('head');
+
+var elementToWait = $(".g-item-sortable[data-id]").length ? ".g-item-sortable[data-id]" : ".awl-item-wrapper[data-price]";
+waitForKeyElements (elementToWait, addItemPricesSum, false);
